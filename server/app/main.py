ae22a2aba7608d64e55c35c04623dbc687d42a36
Abhirup Ghosh <codewithabhi2024@gmail.com>
2026-08-23 20:18:27 +0530

    chore: add requirements.txt

--- server/app/main.py
+++ server/app/main.py
@@ -16,6 +16,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://pavexa.vercel.app",
         "http://localhost:3000",
     ],
     allow_credentials=True,
